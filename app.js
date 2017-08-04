39b17cb448e40ded00856adb04f9569f58fa230b
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-04 11:32:29 -0500

    modules 1 and two working and linked

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ console.log('Server is running');
 
 var randomNumber = require('./randomNumber.js');
 
+var convertToDollars = require('./convertToDollars.js')
+
 console.log(randomNumber(100,1000000));
+console.log(convertToDollars(432.1545678));
--- convertToDollars.js
+++ convertToDollars.js
@@ -1,7 +1,9 @@
 //make a function that accepts a number input and returns USD value
 function makeMoney(inputValue){
     var monies = inputValue.toFixed(2);
-    
+
     return '$' + monies;
 }
-console.log(makeMoney(123456798.1409587968765));
+//console.log(makeMoney(123456798.1409587968765));
+
+module.exports = makeMoney;
