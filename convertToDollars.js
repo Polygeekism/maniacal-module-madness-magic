9f3323c5fbfc55355860b38c5bacd2fa9fdd75dd
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-04 11:40:34 -0500

    modified toDollars to include commas

--- convertToDollars.js
+++ convertToDollars.js
@@ -1,7 +1,8 @@
 //make a function that accepts a number input and returns USD value
-function makeMoney(inputValue){
-    var monies = inputValue.toFixed(2);
-
+function makeMoney(inputValue) {
+    var monies = Number(parseFloat(inputValue).toFixed(2)).toLocaleString('en', {
+        minimumFractionDigits: 2
+    });
     return '$' + monies;
 }
 //console.log(makeMoney(123456798.1409587968765));
